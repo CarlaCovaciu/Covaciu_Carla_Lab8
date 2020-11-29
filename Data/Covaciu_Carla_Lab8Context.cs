cd28e42dda4b397acda10c18120f05aa8896c9f5
Covaciu Carla <covaciu_carla99@yahoo.com>
2020-11-29 20:51:35 +0200

    Add project files.

---
+++ Data/Covaciu_Carla_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Covaciu_Carla_Lab8.Models;
+
+namespace Covaciu_Carla_Lab8.Data
+{
+    public class Covaciu_Carla_Lab8Context : DbContext
+    {
+        public Covaciu_Carla_Lab8Context (DbContextOptions<Covaciu_Carla_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Covaciu_Carla_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Covaciu_Carla_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Covaciu_Carla_Lab8.Data;
+using Covaciu_Carla_Lab8.Models;
+
+namespace Covaciu_Carla_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Covaciu_Carla_Lab8.Data.Covaciu_Carla_Lab8Context _context;
+
+        public IndexModel(Covaciu_Carla_Lab8.Data.Covaciu_Carla_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
